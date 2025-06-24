527004fbd76be8d385f65d3248550ed623c8db0c
Shane Sicienski <sicienss@gmail.com>
2025-06-24 16:33:14 -0400

    Stubbed out keyboard and methods for submitting match code

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,8 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField] GameObject enterMatchCodeScreen;
+    [SerializeField] TMP_Text inputField;
+    [SerializeField] List<Button> keyboardButtons = new List<Button>();
+
+    private void Awake()
+    {
+        // Subscribe
+        foreach (Button button in keyboardButtons)
+        {
+            button.onClick.AddListener(OnKeyboardButtonClick);
+        }
+    }
+
+    private void Start()
+    {
+        // Initialize
+        enterMatchCodeScreen.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        // Unsubscribe
+        foreach (Button button in keyboardButtons)
+        {
+            button.onClick.AddListener(OnKeyboardButtonClick);
+        }
+    }
+
     public void OnSoloPlayClicked()
     {
         // TODO
@@ -19,9 +53,78 @@ public class MainMenuController : MonoBehaviour
 
     public void OnJoinMatchClicked()
     {
-        // Show code entry UI, then:
-        // Realtime.Connect(code);
+        // Show match code entry UI
+        OpenEnterMatchCodeScreen();
+    }
 
+
+    void OpenEnterMatchCodeScreen()
+    {
+        // Initialize screen
+        inputField.text = "";
+
+        enterMatchCodeScreen.SetActive(true);
+    }
+
+    void CloseEnterMatchCodeScreen()
+    {
+        inputField.text = "";
+        enterMatchCodeScreen.SetActive(false);
+    }
+
+    void OnKeyboardButtonClick()
+    {
+        // Get the button that was clicked
+        Button clickedButton = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject?.GetComponent<Button>();
+        if (clickedButton == null)
+            return;
+
+        // Find the TMP_Text child
+        TMP_Text label = clickedButton.GetComponentInChildren<TMP_Text>();
+        if (label == null)
+            return;
+
+        string key = label.text;
+
+        if (key.Equals("DEL", StringComparison.OrdinalIgnoreCase))
+        {
+            if (inputField.text.Length > 0)
+                inputField.text = inputField.text.Substring(0, inputField.text.Length - 1);
+        }
+        else if (key.Equals("CLR", StringComparison.OrdinalIgnoreCase))
+        {
+            inputField.text = "";
+        }
+        else
+        {
+            inputField.text += key;
+        }
+    }
+
+    public void GoBackFromMatchCode()
+    {
+        CloseEnterMatchCodeScreen();   
+    }
+
+    public void SubmitMatchCode()
+    {
+        string input = inputField.text;
+
+        // TODO: logic to check if match code corresponds to Normcore room (or whatever)
+        if (input.Equals("test", StringComparison.OrdinalIgnoreCase))
+        {
+            Debug.Log("Match code success");
+            GoToLobby();            
+        }
+        else
+        {
+            Debug.Log("Match code fail");
+            GoBackFromMatchCode();
+        }
+    }
+
+    public void GoToLobby()
+    {
         // Load scene
         SceneManager.LoadScene("LobbyScene");
     }
